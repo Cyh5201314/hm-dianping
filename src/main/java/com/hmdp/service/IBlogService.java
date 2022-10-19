fccd6f6c498ee89a816d8c8845129797a9334ae8
Jing <504897586@qq.com>
2022-10-19 20:10:36 +0800

    Geo数据结构实现距离排序

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -14,15 +14,45 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 根据博客Id查询信息
+     * @param id
+     * @return
+     */
     Result queryBlogById(Long id);
 
+    /**
+     * 给探店笔记点赞
+     * @param id
+     * @return
+     */
     Result likeBlog(Long id);
 
+    /**
+     * 探店笔记是否被点赞
+     * @param blog
+     */
     void isBlogLiked(Blog blog);
 
+    /**
+     * 设置博客对应的用户信息
+     * @param blog
+     */
     void queryBlogUser(Blog blog);
 
+    /**
+     * 探店笔记的点赞列表
+     * @param id 博客Id
+     * @return
+     */
     Result queryBolgLikes(Long id);
 
+    /**
+     * 新增探店笔记
+     * @param blog
+     * @return
+     */
     Result saveBlog(Blog blog);
+
+    Result queryBlogOfFollow(Long max, Integer offset);
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,15 +1,23 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -19,6 +27,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedisIdWorker redisIdWorker;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     //线程池
     private static final ExecutorService es
@@ -46,6 +57,34 @@ class HmDianPingApplicationTests {
         latch.await();
         long end = System.currentTimeMillis();
         System.out.println("time = " + (end - begin));
+    }
+
+    /**
+     * 将数据库的商铺按照类型,用GEO数据类型,存入Redis中
+     */
+    @Test
+    void  loadShopData(){
+        //1.查询店铺信息
+        List<Shop> list = shopService.list();
+        //2.把店铺分组
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        //3.分批写入redis中
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            //获取类型Id
+            Long typeId = entry.getKey();
+            String key = "shop:geo"+typeId;
+            //获取相同的店铺集合
+            List<Shop> shops = entry.getValue();
+            //写入redis  GEOADD  key 经度 纬度 member(也是店铺Id)
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(shops.size());
+            for (Shop shop : shops) {
+                locations.add(new RedisGeoCommands.GeoLocation<>(shop.getId().toString()
+                ,new Point(shop.getX(),shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key,locations);
+
+        }
+
     }
 
 
